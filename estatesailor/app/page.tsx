8695e7f8b0e54c7210ba358b6f283eb290c3ded3
Brendon Carroll <brendonjcarroll@gmail.com>
2024-08-06 02:11:18 -0700

    Fixed overlay issues

--- estatesailor/app/page.tsx
+++ estatesailor/app/page.tsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-      
+      <div className="space-y-2">
+                  <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none text-black">
+                    Discover the Perfect Estate Sale
+                  </h1>
+                  <p className="max-w-[600px] text-[#000000]">
+                    Our call robot is here to guide you through the exciting world of estate sales. Find hidden gems and
+                    uncover treasures at our upcoming events.
+                  </p>
+                </div>
     </main>
   );
 }
